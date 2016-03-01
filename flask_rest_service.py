5041bc5189b3dd2bf38ea7c0a21ba11ee03b605f
Javier Rey <jreyro@gmail.com>
2016-02-29 20:14:07 -0500

    add list of data scientists

--- flask_rest_service.py
+++ flask_rest_service.py
@@ -1,20 +1,21 @@
 from flask import Flask
+from urllib.request import Request, urlopen
+import json
+
 app = Flask(__name__) 
-# Create the appropriate app.route functions. Test and see if they work
 
-#Make an app.route() decorator here for when the client sends the URI "/puppies"
 
-@app.route('/puppies')
-def puppiesFunction():
-  return "Yes, puppies!"
+@app.route('/dataScientists')
+def dataScientistsFunction():
+	scientists = []
+    url = "https://api.import.io/store/connector/_magic?url=http%3A%2F%2Fwww.meetup.com%2Fes-ES%2FBig-Data-Science-Bogota%2Fmembers%2F&format=JSON&js=false&_apikey=fd51ad66a13d44e1b94f624a191aa2073756fd91206e78e97b26a0898bc3ba7d6ae5bb415ca07421e4eb9dfda36ab684e1b106e7daa3fb9ae49b9b4352e4069ef8e6bc739b4b46b0438f251e1a1986fc"
+    html = urlopen(url).read()
+    data = json.loads(html.decode())
+    for item in data["tables"]:
+        for result in item["results"]:
+             scientists.append(result["memname_link/_text"])
+    return scientists
   
- 
-#Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
-
-@app.route('/puppies/<int:id>')
-def puppiesFunctionId(id):
-  return "This method will act on the puppy with id %s" % id
-
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)	
