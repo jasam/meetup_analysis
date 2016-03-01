518e4339f79264b846205fe920177405368c3122
Javier Rey <jreyro@gmail.com>
2016-02-29 20:17:56 -0500

    fix on code

--- flask_rest_service.py
+++ flask_rest_service.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/dataScientists')
 def dataScientistsFunction():
-	scientists = []
+    scientists = []
     url = "https://api.import.io/store/connector/_magic?url=http%3A%2F%2Fwww.meetup.com%2Fes-ES%2FBig-Data-Science-Bogota%2Fmembers%2F&format=JSON&js=false&_apikey=fd51ad66a13d44e1b94f624a191aa2073756fd91206e78e97b26a0898bc3ba7d6ae5bb415ca07421e4eb9dfda36ab684e1b106e7daa3fb9ae49b9b4352e4069ef8e6bc739b4b46b0438f251e1a1986fc"
     html = urlopen(url).read()
     data = json.loads(html.decode())
