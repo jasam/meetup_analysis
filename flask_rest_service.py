d218b8907205072176bf6ecf7e61a1121245f38b
Javier Rey <jreyro@gmail.com>
2016-03-03 10:40:43 -0500

    fixes

--- flask_rest_service.py
+++ flask_rest_service.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def getDataScientistsFunction():
     scientists = []
     data_scientists = {}
-    qx_data_scientist = 1063
+    qx_data_scientist = 0
     quantity_pages = qx_data_scientist / 20
     url = "https://api.import.io/store/connector/_magic?url=http%3A%2F%2Fwww.meetup.com%2FBig-Data-Science-Bogota%2Fmembers%2F%3Foffset%3D{}%26sort%3Djoin_date%26desc%3D1&format=JSON&js=false&_apikey=fd51ad66a13d44e1b94f624a191aa2073756fd91206e78e97b26a0898bc3ba7d6ae5bb415ca07421e4eb9dfda36ab684e1b106e7daa3fb9ae49b9b4352e4069ef8e6bc739b4b46b0438f251e1a1986fc"
     member_number = 0
